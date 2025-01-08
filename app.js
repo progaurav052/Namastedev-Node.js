f7a2585aa7da3675c44787a074d5d826d1f7eb1e
Gaurav <gaurav511pai@gmail.com>
2025-01-08 21:57:22 +0530

    Writing first Node.js code and concept of globalThis , global

---
+++ app.js
@@ -0,0 +1,12 @@
+var n ="Namaste Node.js"
+var a=10;
+var b = 20;
+console.log(n);
+console.log(a);
+console.log(b);
+console.log(global); // this is global object provided by node.js and not v8 engine , has setTimeout , setInterval etc which are some superpowers 
+// in web browser we use window object which is also global 
+console.log(globalThis); // to make an standard  , because before this it was referred by many names on browsers , webworkers , node 
+// so an standard is made , same as globalThis , which is supported accross all now 
+
+
